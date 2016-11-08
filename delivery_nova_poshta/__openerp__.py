697fc10400a3defebf4db6c8c68185ed982a8ed6
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-11-08 14:48:23 +0200

    [FIX] add author info

--- delivery_nova_poshta/__openerp__.py
+++ delivery_nova_poshta/__openerp__.py
@@ -5,6 +5,8 @@
     'category': 'Sales Management',
     'summary': 'Odoo - Nova Poshta Shipping Integration',
     'version': '1.0',
+    'author': "ERP Ukraine",
+    'website': "https://erp.co.ua",
     'depends': ['delivery'],
     'data': [
         'views/np_delivery_carrier.xml',
@@ -12,6 +14,5 @@
         'data/np_models_data.xml',
         'security/ir.model.access.csv'
     ],
-    'installable': True,
     'application': True,
 }
--- website_delivery_nova_poshta/__openerp__.py
+++ website_delivery_nova_poshta/__openerp__.py
@@ -5,11 +5,12 @@
     'category': 'Website',
     'summary': 'Nova Poshta Shipping Integration for Website Sales',
     'version': '1.0',
+    'author': "ERP Ukraine",
+    'website': "https://erp.co.ua",
     'depends': ['website_sale_delivery', 'delivery_nova_poshta'],
     'data': [
         'views/templates.xml',
     ],
     'js': ['static/src/js/website_delivery_nova_poshta.js'],
-    'installable': True,
     'application': True,
 }
